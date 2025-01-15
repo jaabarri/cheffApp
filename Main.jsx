1aecec29acdd006c246ef6940f92e8bd82e17e28
jaabarri <jaabarri1112@gmail.com>
2025-01-14 20:55:50 -0800

    Ingredients component

--- Main.jsx
+++ Main.jsx
@@ -1,14 +1,11 @@
 import React from "react" 
 import Recipe from "./Recipe"
+import Ingredients from "./Ingredients"
 
 export default function Main() {
     const [ingredients, setIngredients] = React.useState(["all the main spices", "pasta", "ground beef", "tomato paste"])
     const [state, setState] = React.useState(false)
     
-    const ingredientsListItems = ingredients.map(ingredient => (
-        <li key={ingredient}>{ingredient}</li>
-    ))
-
     function ingredientList(formData) {
         const newIngredient = formData.get("ingredient")
         setIngredients(prevIngredients=> [...prevIngredients, newIngredient])
@@ -30,17 +27,11 @@ export default function Main() {
                 <button>Add ingredient</button>
             </form>
 
-            {ingredients.length ? <section>
-                <h2>Ingredients on hand:</h2>
-                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
-                {ingredients.length > 3 ? <div className="get-recipe-container">
-                    <div>
-                        <h3>Ready for a recipe?</h3>
-                        <p>Generate a recipe from your list of ingredients.</p>
-                    </div>
-                    <button onClick={shownState}>Get a recipe</button>
-                </div> : null}
-            </section> : null}
+            {ingredients.length ? 
+                <Ingredients 
+                    ingredients={ingredients}
+                    shownState={shownState}
+                /> : null}
 
             {state ? <Recipe /> : null }
 
